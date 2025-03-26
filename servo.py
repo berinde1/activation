f9a34f869a0f4c2670a7c47092e9b2b6b62672e3
Maya Berinde <maya.berinde@mail.utoronto.ca>
2025-03-26 12:26:19 -0400

    tested servo code from lab 4

---
+++ servo.py
@@ -0,0 +1,32 @@
+'''
+ESC204S Lab 3, Task F
+Task: Run a continuous-rotation servo using the L298N driver.
+'''
+
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials Servo standard servo example"""
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+print("Servo shaft is turning now.")
+
+while True:
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+
+# to test: rotating at larger angle rotations to see what is ideal for the brush
+
